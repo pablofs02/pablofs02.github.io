c61b5726df83e75950d15dc06605f0c22705205f
TetrisRubik <fpablo960@gmail.com>
2022-09-28 14:29:42 +0200

    Imagenes Todos los repositorios están representados mediante una imagen.

--- func.js
+++ func.js
@@ -3,11 +3,11 @@ const elementos = [
 		nombre: "Aplicaciones",
 		artículos: [{
 			nombre: "Cronos",
-			imagen: "https://avatars.githubusercontent.com/u/76429811",
+			imagen: "https://repository-images.githubusercontent.com/509259259/f031b98d-c95d-42a1-807a-f3c0f13a8be9",
 			enlace: "Cronos"
 		}, {
 			nombre: "Curriculum Vitae",
-			imagen: "https://avatars.githubusercontent.com/u/76429811",
+			imagen: "https://repository-images.githubusercontent.com/465828758/247802b1-291c-42f3-9f4c-b0c765b5e640",
 			enlace: "Curriculum-Vitae"
 		}]
 	},
@@ -19,11 +19,11 @@ const elementos = [
 			enlace: "Rompe-Ladrillos"
 		}, {
 			nombre: "Lucha",
-			imagen: "https://avatars.githubusercontent.com/u/76429811",
+			imagen: "https://repository-images.githubusercontent.com/487407324/4635a82d-82e8-4f07-acb8-fbea30778ad0",
 			enlace: "Lucha"
 		}, {
 			nombre: "Serpiente",
-			imagen: "https://avatars.githubusercontent.com/u/76429811",
+			imagen: "https://repository-images.githubusercontent.com/469462989/c32df3e4-c1cf-4d79-970a-257dbadcf401",
 			enlace: "https://github.com/TetrisRubik/Serpiente"
 		}]
 	}];
